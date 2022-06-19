38f037401cbad8608a10bf0ae58348929e37d00b
ShulpinP <pavelshulpin6@gmail.com>
2022-06-19 21:34:12 +0300

    casting

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task6;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first = 1000;
-        int second = 100;
+        int second = (int) first;
         System.out.println(first);
         System.out.println(second);
     }
